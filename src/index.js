d85a89a3d2e4f8a3bc878abc724ae47277d900d9
John Lenin Ortiz <lenincomp@gmail.com>
2021-02-01 15:34:02 -0500

    deploy

--- src/index.js
+++ src/index.js
@@ -14,7 +14,9 @@ const store = createStore(
   reducers, 
   compose(
     applyMiddleware(sagaMiddleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    window.__REDUX_DEVTOOLS_EXTENSION__
+    ? window.__REDUX_DEVTOOLS_EXTENSION__()
+    : f => f
   )
 );
 sagaMiddleware.run(rootSaga);
