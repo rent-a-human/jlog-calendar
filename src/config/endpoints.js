e713e6d387113f930fe0b97bd20fadf119d399d7
John Lenin Ortiz <lenincomp@gmail.com>
2021-10-24 18:38:49 -0500

    Update endpoints.js

--- src/config/endpoints.js
+++ src/config/endpoints.js
@@ -1,3 +1,3 @@
 export const FORECAST_API_BASE_URL =
   process.env.REACT_APP_FORECAST_API_BASE_URL ??
-  'http://api.weatherapi.com/v1/'; // http://api.weatherapi.com/v1/current.json?key=
+  'https://api.weatherapi.com/v1/'; // http://api.weatherapi.com/v1/current.json?key=
